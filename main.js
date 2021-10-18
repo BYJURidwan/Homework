117b6589b53efb55120b8b715606b41b2cf86644
BYJURidwan <89031237+BYJURidwan@users.noreply.github.com>
2021-10-17 20:07:27 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var a = Math.random(Math.round(1, 12));
+var b = Math.random(Math.round(1, 12));
+console.log(a);
+console.log(b);
+var add = a + b;
+var sub = a - b;
+var mul = a * b;
+var div = Math.round(a / b);
+console.log(add);
+console.log(sub);
+console.log(mul);
+console.log(div);
